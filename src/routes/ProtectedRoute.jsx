29cf9a6df818ded490e20089ef1cf17a4ce47519
Faturahman <faturrahmau@gmail.com>
2025-06-28 10:00:16 +0700

    a

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,7 +3,12 @@ import { Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 export default function ProtectedRoute({ children, allowedRole }) {
+  const token = Cookies.get("token");
   const role = Cookies.get("role");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
 
   if (allowedRole && role !== allowedRole) {
     alert("❌ Kamu tidak memiliki akses ke halaman ini.");
